c7e6f55da8ed853582a756d3f98558d18658c33d
Abdulkareem Mustapha <aphatheology@gmail.com>
2020-08-16 22:05:01 +0100

    Update syle to include number

--- index.js
+++ index.js
@@ -1,43 +1,4 @@
-
-
-//function for form validation
-function validation(){
-
-
-    var first_name  = document.getElementById("first-name").value.trim()
-    var last_name = document.getElementById("last-name").value.trim()
-    var email = document.getElementById("email").value.trim() 
-    var phone = document.getElementById("phone").value.trim()
-    var error_text = document.getElementById("error-id")
-    var display
-
-
-    //conditional statements and decision making   
-    if (first_name.length < 3){
-        display = "Enter a valid first name"
-        error_text.innerHTML = display
-        return false
-    }
-
-    if (email.indexOf("@") == -1 || email.indexOf("@") == 0 || email.length < 7){
-        display = "Enter a valid email address"
-        error_text.innerHTML = display
-        return false
-    }
-
-    if (last_name.length < 3){
-        display = "Enter a valid last name"
-        error_text.innerHTML = display
-        return false
-    }
-
-    if (phone.length < 9 || phone.length > 15 || isNaN(phone)){
-        display = "Enter a valid phone number"
-        error_text.innerHTML = display
-        return false
-    }
-
-alert("Form successfully submitted")
-return true    
-
-}
+/**
+ * Exposing intl-tel-input as a component
+ */
+module.exports = require("./build/js/intlTelInput");
---
+++ script.js
@@ -0,0 +1,55 @@
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+        initialCountry: "auto",
+        geoIpLookup: function(success, failure) {
+        $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+        success(countryCode);
+    });
+},
+    utilsScript: "build/js/utils.js",
+});
+
+//function for form validation
+function validation(){
+
+
+    var first_name  = document.getElementById("first-name").value.trim()
+    var last_name = document.getElementById("last-name").value.trim()
+    var email = document.getElementById("email").value.trim() 
+    var phone = document.getElementById("phone").value.trim()
+    var error_text = document.getElementById("error-id")
+    var display
+
+
+    //conditional statements and decision making   
+    if (first_name.length < 3){
+        display = "Enter a valid first name"
+        error_text.innerHTML = display
+        return false
+    }
+
+    if (email.indexOf("@") == -1 || email.indexOf("@") == 0 || email.length < 7){
+        display = "Enter a valid email address"
+        error_text.innerHTML = display
+        return false
+    }
+
+    if (last_name.length < 3){
+        display = "Enter a valid last name"
+        error_text.innerHTML = display
+        return false
+    }
+
+    if (phone.length < 9 || phone.length > 15 || isNaN(phone)){
+        display = "Enter a valid phone number"
+        error_text.innerHTML = display
+        return false
+    }
+
+alert("Form successfully submitted")
+return true    
+
+}
+
+module.exports = require("./build/js/intlTelInput");
